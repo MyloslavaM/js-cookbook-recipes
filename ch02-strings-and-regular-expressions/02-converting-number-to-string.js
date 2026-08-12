e3659ff701ea3219b2d38362ff52c2e266ed2090
Myloslava Martynova <myloslava.martynova@gmail.com>
2026-08-12 12:53:29 +0300

    feat(string): continue working on converting numbers to string

--- ch02-strings-and-regular-expressions/02-converting-number-to-string.js
+++ ch02-strings-and-regular-expressions/02-converting-number-to-string.js
@@ -46,4 +46,48 @@ console.assert(
 );
 console.log("all assertions for stringConvert2 are PASS");
 
-//
+/*JS has 3 utility methods build into the number data type.
+Number.toFixed()
+Number.toEExponential()
+Number.toPrecision()
+*/
+
+//======== practice ========
+const someNumber = 1234.0055;
+
+//Ask for exactly two decimal points. Numbers will rounded if necessary
+const fixedString = someNumber.toFixed(2);
+console.assert(
+  fixedString.length === 7,
+  "The length of someNumder should be 7 (1234.01)",
+);
+
+// Ask for 5 significant digits. TBD
+const precisionString = someNumber.toPrecision(5);
+
+console.assert(
+  precisionString.length === 6,
+  "The length of someNumber should be 6 (1234.0)",
+);
+
+// Ask for scientific notation with 2 decimal plates. TBD
+const scientificString = someNumber.toExponential(2); // => scientificString === 1.23e+3
+
+// Format number as US currency string
+//======== practice ========
+const moneyString = (number) => {
+  const formatter = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return formatter.format(number);
+};
+console.assert(
+  typeof moneyString(123) === "string",
+  "The type of stringConvert should be string",
+);
+console.assert(
+  moneyString(5_000).length === 9,
+  "The length of moneyString(5_000) should be 9 => $5,000.00",
+);
+console.log("all assertions for moneyString function are PASS");
