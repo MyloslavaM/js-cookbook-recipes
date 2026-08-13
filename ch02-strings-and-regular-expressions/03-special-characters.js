ff451f182e140fe03c21fce8cba0b28878d2773d
Myloslava Martynova <myloslava.martynova@gmail.com>
2026-08-13 12:09:22 +0300

    fix(string): adding assertions

--- ch02-strings-and-regular-expressions/03-special-characters.js
+++ ch02-strings-and-regular-expressions/03-special-characters.js
@@ -22,14 +22,21 @@ console.log(copyrightNotice);
 //Emojis it is extended Unicode characteristics
 const hamburger = "🍔"; // copy-paste method
 const hamburgerStory = "I like hamburgers " + hamburger;
-// console.log(`🍔 length: ${hamburger.length}`);
-// console.log(hamburgerStory);
+console.assert(hamburger.length === 2, "Hamburger emoji must have length 2");
+console.assert(
+  typeof hamburgerStory === "string",
+  "The type of hamburgerStory should be string",
+);
+console.log(`🍔 length: ${hamburger.length}`); // => 2- because the hamburger emoji take twice as many bites in memory .
 
 /* Another method is to use the Unicode value for emojis.
 The problem you can't use a standard \u 
 => every emoji is stored as s 4-byte value
 => the unicode characters that map to the keys are usually encoded as 2-byte value
+
+the solution is to use the String.fromCodePoint() method
 */
 
 const hamburgerStory1 = `I like hamburgers like ${hamburger} and ${String.fromCodePoint(0x1f354)}`;
+// the hamburger emoji has the hexadecimal code U+1F354. To use it with .fromCodePoint(), replace the prefix U+ with 0x
 console.log(hamburgerStory1);
