9daaa11baf714858a61c5702f1d4f75495c35471
Myloslava Martynova <myloslava.martynova@gmail.com>
2026-08-22 18:19:02 +0300

    refactor: update random number generation to use cryptographic methods and add tests

---
+++ ch03-numbers/02-crypto-random-numder.test.js
@@ -0,0 +1,15 @@
+import { randomNumber } from "./02-crypto-random-number.mjs";
+
+const ranges = [
+  { min: 1, max: 2 },
+  { min: 1, max: 6 },
+  { min: 1, max: 100 },
+];
+test.each(ranges)(
+  `return a number between min and max values`,
+  ({ min, max }) => {
+    const result = randomNumber(min, max);
+    expect(result).toBeGreaterThanOrEqual(min);
+    expect(result).toBeLessThanOrEqual(max);
+  },
+);
