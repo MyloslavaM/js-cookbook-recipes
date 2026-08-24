fc9554c027177b6568eeb961c24800ff8086fbec
Myloslava Martynova <myloslava.martynova@gmail.com>
2026-08-24 12:40:13 +0300

    feat(array): add validation for checking if an object is an array

---
+++ ch05-array/01-check-if-odject-is-array.js
@@ -0,0 +1,9 @@
+//************* COMPENDIUM ********************* */
+/*
+Before you perform an array operation, you want to verify that your object truly is an array 
+Array.isArray()
+ */
+const browserNames = ["Firefox", "Edge", "Chrome", "IE", "Safari"];
+if (Array.isArray(browserNames)) {
+  // browserNames is valid array
+}
