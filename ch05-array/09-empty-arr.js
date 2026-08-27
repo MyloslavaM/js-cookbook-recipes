f780603f46c92abeec16e9c0fb88f7a981944d80
Myloslava Martynova <myloslava.martynova@gmail.com>
2026-08-27 12:18:07 +0300

    feat(array): update example for removing all elements from an array with clearer variable names and additional context

--- ch05-array/09-empty-arr.js
+++ ch05-array/09-empty-arr.js
@@ -3,7 +3,14 @@
 You need to remove all elements from an arr, either to reclaim memory or so that your arr can be reused
 set the length property of your arr to 0
  */
-const numbers = [2, 42, 5, , 67, 190];
-numbers.length = 0;
+const numbersA = [2, 42, 5, 43, 67, 190];
+numbersA.length = 0; // []
 
-console.log(numbers); // []
+console.log(numbersA); // []
+
+const numbersB = [2, 42, 5, 43, 67, 190];
+numbersB.length = 3; // [2, 42, 5]
+
+console.log(numbersB); // [2, 42, 5]
+
+// Array.pop()- removes the last element from arr
